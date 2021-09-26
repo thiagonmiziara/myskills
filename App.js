2e2f1e514b8c24743efaa916b0e1805083d189c4
Thiago Miziara <thiagonmiziara@gmail.com>
2021-09-26 15:19:39 -0300

    add primary project react-native ignite

---
+++ App.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>React Native</Text>
+    </View>
+  );
+}
