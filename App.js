3cae59ebf39680450bf37aa3c8a9105598b29fd7
Thiago Miziara <thiagonmiziara@gmail.com>
2021-09-26 16:02:42 -0300

    add primary component

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Home } from './src/pages/Home';
 
 export default function App() {
-  return (
-    // eslint-disable-next-line react-native/no-inline-styles
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>React Native</Text>
-    </View>
-  );
+  return <Home />;
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+   View, 
+   Text, 
+   StyleSheet, 
+   TextInput, 
+   Platform,
+   TouchableOpacity
+} from 'react-native';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Welcome, Thiago
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor="#555"
+      />
+
+      <TouchableOpacity 
+        style={styles.button}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.buttonText}>
+          Add
+        </Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, {marginTop: 50}]}>
+        My Skills
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title:{
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input:{
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button:{
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText:{
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 17,
+  }
+})
