dd8f84695e217f08447f3eb3bc137a4a6c2255e4
Thiago Miziara <thiagonmiziara@gmail.com>
2021-09-27 19:03:53 -0300

    add components button and skillcard & FlatList para lidar com listas grandes no react-native

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,106 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-   View, 
-   Text, 
-   StyleSheet, 
-   TextInput, 
-   Platform,
-   TouchableOpacity
-} from 'react-native';
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+} from "react-native";
+import { Button } from "../components/Button";
+import { SkillCard } from "../components/SkillCard";
 
 export function Home() {
-  const [newSkill,setNewSkill] = useState('');
+  const [newSkill, setNewSkill] = useState("");
   const [mySkills, setMySkills] = useState([]);
 
-  function handleAddNewSkill(){
-    setMySkills(oldState => [...oldState, newSkill]);
+  function handleAddNewSkill() {
+    setMySkills((oldState) => [...oldState, newSkill]);
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Welcome, Thiago
-      </Text>
+      <Text style={styles.title}>Welcome, Thiago</Text>
 
       <TextInput
         style={styles.input}
-        placeholder="New Skill"
-        placeholderTextColor="#555"
+        placeholder='New Skill'
+        placeholderTextColor='#555'
         onChangeText={setNewSkill}
       />
 
-      <TouchableOpacity 
-        style={styles.button}
-        activeOpacity={0.7}
-        onPress={handleAddNewSkill}
-      >
-        <Text style={styles.buttonText}>
-          Add
-        </Text>
-      </TouchableOpacity>
+      <Button onPress={handleAddNewSkill} />
 
-      <Text style={[styles.title, {marginVertical: 50}]}>
-        My Skills
-      </Text>
-
-      {mySkills.map((skill, index) =>{
-        return(
-          <TouchableOpacity style={styles.buttonSkill} key={`SKILL=${index}`}>
-            <Text style={styles.textSkill}>
-              {skill}
-           </Text>
-          </TouchableOpacity>
-        )
-        })
-      }  
-
+      <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item}
+        renderItem={({item})=> (
+          <SkillCard skill={item} />
+        )}
+      />
+       
+    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor: '#121015',
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
     paddingHorizontal: 20,
     paddingVertical: 70,
-    paddingHorizontal: 30
+    paddingHorizontal: 30,
   },
-  title:{
-    color: '#fff',
+  title: {
+    color: "#fff",
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
-  input:{
-    backgroundColor: '#1f1e25',
-    color: '#fff',
+  input: {
+    backgroundColor: "#1f1e25",
+    color: "#fff",
     fontSize: 18,
-    padding: Platform.OS === 'ios' ? 15 : 10,
+    padding: Platform.OS === "ios" ? 15 : 10,
     marginTop: 30,
-    borderRadius: 7
-  },
-  button:{
-    backgroundColor: '#a370f7',
-    padding: 15,
     borderRadius: 7,
-    alignItems: 'center',
-    marginTop: 20
   },
-  buttonText:{
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 17,
-  },
-  buttonSkill:{
-    backgroundColor:'#1f1e25',
-    padding: 15,
-    borderRadius: 50,
-    alignItems: 'center',
-    marginVertical: 10
-  },
-  textSkill:{
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  }
-})
+});
